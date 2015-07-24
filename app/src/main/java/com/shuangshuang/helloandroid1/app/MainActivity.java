dc2d7def39738f00c9baf7412d771e786421e3ac
wys <270476971@qq.com>
2015-07-24 15:34:26 +0800

    删除不需要的菜单部分的代码。

--- app/src/main/java/com/shuangshuang/helloandroid1/app/MainActivity.java
+++ app/src/main/java/com/shuangshuang/helloandroid1/app/MainActivity.java
@@ -3,16 +3,22 @@ package com.shuangshuang.helloandroid1.app;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
+
+
+    // this is a comment
+
+    //this is  a comment
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 
 
     @Override
@@ -22,18 +28,14 @@ public class MainActivity extends ActionBarActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
-        return super.onOptionsItemSelected(item);
-    }
+
+
+
+
+
+
+
+
 }
